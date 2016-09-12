b90e1dc505630c385b77e8314542762cf3f9797d
Mario Lamacchia <mario@appstrakt.com>
2016-09-12 23:03:38 +0200

    Include material-ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import styles from './index.scss';
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
-        <p>Global bootstrap css import works too as you can see on the following button.</p>
-        <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <h1>It Works!</h1>
+          <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
+          <p>Global bootstrap css import works too as you can see on the following button.</p>
+          <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
+          <RaisedButton label="Default" />
+        </div>
+      </MuiThemeProvider>
+
     )
   }
 }
