b9f2465333868597e960edbff733d1b747a81bb4
Mario Lamacchia <mario@appstrakt.com>
2016-09-13 00:50:15 +0200

    Include redux and redux-router

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,41 @@
 import styles from './index.scss';
 
 import React from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
 injectTapEventPlugin();
 
 export default class App extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.node
+  };
+
   render() {
     return (
       <MuiThemeProvider>
         <div>
           <h1>It Works!</h1>
-          <p>This React project just works including <span className={styles.blueBg}>module</span> local styles.</p>
-          <p>Global bootstrap css import works too as you can see on the following button.</p>
-          <p><a className="btn btn-primary btn-lg">Enjoy!</a></p>
+            {this.props.children}
           <RaisedButton label="Default" />
         </div>
       </MuiThemeProvider>
+    )
+  }
+}
+export class Component1 extends React.Component {
+  render() {
+    return (
+      <span>c1</span>
+    )
+  }
+}
+export class Component2 extends React.Component {
+  render() {
+    return (
+      <span>c2</span>
     )
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
-import App from './app.jsx';
+import { createHistory } from 'history';
+import { Route } from 'react-router';
+import { combineReducers, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reduxReactRouter, routerStateReducer, ReduxRouter } from 'redux-router';
+import App, { Component1, Component2 } from './app.jsx';
 
-render(<App/>, document.querySelector("#app"));
+class Root extends Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <ReduxRouter>
+            <Route path="/" component={App}>
+              <Route path="1" component={Component1}></Route>
+              <Route path="2" component={Component2}></Route>
+            </Route>
+          </ReduxRouter>
+        </Provider>
+      </div>
+    )
+  }
+}
+
+const reducer = combineReducers({
+  router: routerStateReducer,
+});
+
+const store = compose(
+  reduxReactRouter({ createHistory }),
+  // devTools()
+)(createStore)(reducer);
+
+render(<Root/>, document.querySelector("#app"));
