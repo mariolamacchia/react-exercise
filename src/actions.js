ca2e6ad0ac3f1ed6920dd67d17bb3d5283c504ab
Mario Lamacchia <mario@appstrakt.com>
2016-09-13 23:09:43 +0200

    Get cats images from subreddit

--- src/actions.js
+++ src/actions.js
@@ -24,23 +24,57 @@ function failCatsRequest(error) {
   };
 }
 
+
 export function fetchCats() {
   return function(dispatch) {
     dispatch(requestCats());
 
+    // Get data.json
     fetch('/data.json')
+      .then(parseResponse)
+      // For each breed of cats, take a list from the subreddit
+      .then(json => {
+        let promises = json.breeds.map(breed => {
+          return fetch('https://www.reddit.com/r/' + breed.subreddit + '/.json')
+            .then(parseResponse);
+        });
+        return Promise.all(promises).then(subreddits => {
+          // Store each subreddit response into its cat breed
+          subreddits.forEach((subreddit, index) => {
+            json.breeds[index].images = getImagesFromSubreddit(subreddit);
+          });
+          return json.breeds;
+        });
+      })
       .then(
-        response => {
-          if (response.status >= 400) {
-            return Promise.reject(response.statusText);
-          } else {
-            return response.json();
-          }
-        }
-      )
-      .then(
-        json => dispatch(receiveCats(json.races)),
+        json => dispatch(receiveCats(json)),
         error => dispatch(failCatsRequest(error))
       )
   }
+}
+
+function parseResponse(response) {
+  if (response.status >= 400) {
+    return Promise.reject(response.statusText);
+  } else {
+    return response.json();
+  }
+}
+
+function getImagesFromSubreddit(subreddit) {
+  let items = subreddit.data.children
+    // Filter items with a preview
+    .filter(post => !!(
+      post.data.preview
+    ))
+    // Return the preview source
+    .map(post => {
+      // If for some reason the preview source is missing return 404 image
+      try {
+        return post.data.preview.images[0].source
+      } catch(e) {
+        return { url: 'https://http.cat/404' };
+      }
+    });
+  return items;
 }
