e1b5e491d59a699bdb7e6c6554ce43f342f7d57e
Mario Lamacchia <mario@appstrakt.com>
2016-09-15 00:08:34 +0200

    Add loader

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,22 +4,49 @@
  * Main app component
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from 'material-ui/CircularProgress';
 
 injectTapEventPlugin();
 
-export default class extends React.Component {
-  static propTypes = {
-    children: React.PropTypes.node
-  };
-
+export default class App extends React.Component {
   render() {
+    const { isFetching } = this.props;
+    const loaderStyle = {
+      position: 'fixed',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(255, 255, 255, .8)',
+      zIndex: '3000'
+    };
+
     return (
       <MuiThemeProvider>
-        {this.props.children}
+        <div>
+          {isFetching &&
+            <div style={loaderStyle}><CircularProgress size={2} /></div>
+          }
+          {this.props.children}
+        </div>
       </MuiThemeProvider>
     )
   }
 }
+
+App.propTypes = {
+  children: React.PropTypes.node,
+  isFetching: React.PropTypes.bool.isRequired,
+};
+
+function mapStateToProps(state) {
+  const { isFetching } = state.cats;
+  return { isFetching };
+}
+
+export default connect(mapStateToProps)(App);
